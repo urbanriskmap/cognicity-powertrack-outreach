6a12469e23b80fce7f68bd0aa31af764f27fff50
ojha-url <mayank.ojha@gmail.com>
2018-11-21 14:13:29 -0500

    Included base filename for log output

--- src/index.js
+++ src/index.js
@@ -9,11 +9,15 @@ console.log(config.logger);
 
 let loggerTransports = [];
 if (config.logger.filename) {
+  const baseName = config.logger.filename;
   loggerTransports = [
     // - Write to all logs with level `info` and below to `combined.log`
-    new winston.transports.File({filename: 'error.log', level: 'error'}),
+    new winston.transports.File({
+      filename: baseName + 'error.log',
+      level: 'error',
+    }),
     // - Write all logs error (and below) to `error.log`.
-    new winston.transports.File({filename: 'combined.log'}),
+    new winston.transports.File({filename: baseName + 'combined.log'}),
   ];
 } else {
   loggerTransports = [
